5dd280577468bc416b74715a28c622f90b779fa3
Reynaldo <reynaldo_amed_777@hotmail.com>
2014-09-03 08:22:45 +0200

    primer ejercicio

---
+++ ejercicio1/ejercicio1.py
@@ -0,0 +1,45 @@
+print ("el inciso a del ejercicio 1")
+for principal in range(20):
+	print (principal+1),
+	principal=principal+1
+print " "
+print " "
+
+
+print ("el inciso b del ejercicio 1")
+a=20
+while a >=1:
+	print a,
+	a -= 1
+print " "
+print " "
+
+
+
+print("el inciso c del ejercicio 1")
+def suma(x, y):
+	return  x + y
+a=raw_input("ingrese el primer numero")
+b=raw_input("ingrese el segundo numero")
+value=int(a)
+value1=int(b)
+print suma(value, value1)
+print " "
+
+
+print("el inciso b del ejercicio 1")
+def suma2(x, y):
+	return  x + y
+a=raw_input("ingrese el primer numero")
+b=raw_input("ingrese el segundo numero")
+print suma(a,b)
+print " "
+print " "
+
+
+print("el inciso f del ejercicio 1")
+def suma3(x, y):
+	return  x + y
+a=input("ingrese el priemer nuemero")
+b=input("ingrese el segundo nuemero")
+print suma(a,b)
